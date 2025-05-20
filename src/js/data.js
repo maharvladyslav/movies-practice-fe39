0b13ddd354f4dde768be868567df014c49f73904
Mahar Vladyslav <maharvladyslav@gmail.com>
2025-05-20 17:37:47 +0300

    init commit

---
+++ src/js/data.js
@@ -0,0 +1,189 @@
+export default [
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    description:
+      "Ув'язнений несправедливо чоловік налагоджує дружбу та не втрачає надії, плануючи втечу.",
+    rating: 9.3,
+    poster:
+      "https://m.media-amazon.com/images/S/pv-target-images/cfc4f457cbfd4033e9f46c76aa74611c9d7505f1740005e8ae76efc3a8ec1712.jpg",
+    genre: ["drama", "crime"],
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    description:
+      "Злодій викрадає секрети через сон та отримує завдання вкласти ідею.",
+    rating: 8.8,
+    poster: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SL1500_.jpg",
+    genre: ["action", "sci-fi", "thriller"],
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    description:
+      "Історії гангстерів, боксера та мафіозної родини переплітаються в Лос-Анджелесі.",
+    rating: 8.9,
+    poster:
+      "https://www.emp.co.uk/dw/image/v2/BBQV_PRD/on/demandware.static/-/Sites-master-emp/default/dw1746c00e/images/5/7/7/2/577221b.jpg?sfrm=png",
+    genre: ["crime", "drama"],
+  },
+  {
+    title: "The Matrix",
+    year: 1999,
+    description:
+      "Програміст дізнається, що реальність — це комп’ютерна симуляція.",
+    rating: 8.7,
+    poster: "https://m.media-amazon.com/images/I/51vpnbwFHrL._AC_.jpg",
+    genre: ["action", "sci-fi"],
+  },
+  {
+    title: "Parasite",
+    year: 2019,
+    description:
+      "Бідна родина проникає у життя заможних людей з неочікуваними наслідками.",
+    rating: 8.5,
+    poster:
+      "https://image.tmdb.org/t/p/original/dwkDeJXXaHVuIhcyCxUdb3YctxY.jpg",
+    genre: ["drama", "thriller"],
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    description:
+      "Астронавти шукають новий дім для людства за межами Сонячної системи.",
+    rating: 8.6,
+    poster: "https://m.media-amazon.com/images/I/514zBLkyJcL.jpg",
+    genre: ["adventure", "drama", "sci-fi"],
+  },
+  {
+    title: "Gladiator",
+    year: 2000,
+    description:
+      "Римський генерал стає гладіатором, щоб помститися імператору.",
+    rating: 8.5,
+    poster: "https://m.media-amazon.com/images/I/912Ht9rUWWS.jpg",
+    genre: ["action", "drama"],
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    description: "Історія мафіозної родини Корлеоне в післявоєнній Америці.",
+    rating: 9.2,
+    poster: "https://m.media-amazon.com/images/I/51rOnIjLqzL._AC_.jpg",
+    genre: ["crime", "drama"],
+  },
+  {
+    title: "La La Land",
+    year: 2016,
+    description:
+      "Музичний роман між джазовим піаністом і акторкою в Лос-Анджелесі.",
+    rating: 8.0,
+    poster: "https://m.media-amazon.com/images/I/91jrKX9xjQL._AC_SL1500_.jpg",
+    genre: ["drama", "music", "romance"],
+  },
+  {
+    title: "Spirited Away",
+    year: 2001,
+    description:
+      "Дівчинка потрапляє до чарівного світу духів та намагається врятувати своїх батьків.",
+    rating: 8.6,
+    poster:
+      "https://m.media-amazon.com/images/I/61ON14PVzoL._AC_UF894,1000_QL80_.jpg",
+    genre: ["animation", "adventure", "family"],
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    description: "Бетмен протистоїть Джокеру, що тероризує місто Готем.",
+    rating: 9.0,
+    poster:
+      "https://m.media-amazon.com/images/I/5151N2hUPiL._AC_UF894,1000_QL80_.jpg",
+    genre: ["action", "crime", "drama"],
+  },
+  {
+    title: "Whiplash",
+    year: 2014,
+    description:
+      "Молодий барабанщик прагне величі під жорстким наглядом викладача.",
+    rating: 8.5,
+    poster: "https://i.ebayimg.com/images/g/5PgAAOSwEKRhZOs7/s-l1200.jpg",
+    genre: ["drama", "music"],
+  },
+  {
+    title: "The Grand Budapest Hotel",
+    year: 2014,
+    description:
+      "Пригоди ексцентричного консьєржа та юного помічника в готелі.",
+    rating: 8.1,
+    poster: "https://printler.com/media/photo/107028.jpg",
+    genre: ["adventure", "comedy", "crime"],
+  },
+  {
+    title: "Fight Club",
+    year: 1999,
+    description:
+      "Білий комірець створює підпільний клуб боїв із харизматичним незнайомцем.",
+    rating: 8.8,
+    poster: "https://content2.rozetka.com.ua/goods/images/big/236096129.jpg",
+    genre: ["drama"],
+  },
+  {
+    title: "WALL·E",
+    year: 2008,
+    description:
+      "Маленький робот прибиральник мріє про любов у постапокаліптичному майбутньому.",
+    rating: 8.4,
+    poster:
+      "https://www.tallengestore.com/cdn/shop/products/WALL_E-HollywoodAnimationClassicMoviePoster_e1aa7d6e-de2c-4476-9793-e2c7dd74e6aa.jpg?v=1591603143",
+    genre: ["animation", "adventure", "family"],
+  },
+  {
+    title: "Coco",
+    year: 2017,
+    description:
+      "Музично обдарований хлопчик потрапляє до світу мертвих, шукаючи правду про родину.",
+    rating: 8.4,
+    poster: "https://cdn.europosters.eu/image/750/56183.jpg",
+    genre: ["animation", "adventure", "family"],
+  },
+  {
+    title: "Django Unchained",
+    year: 2012,
+    description:
+      "Звільнений раб шукає дружину, яку утримує безжальний плантатор.",
+    rating: 8.5,
+    poster: "https://content1.rozetka.com.ua/goods/images/big/434339583.jpg",
+    genre: ["drama", "western"],
+  },
+  {
+    title: "Her",
+    year: 2013,
+    description:
+      "Самотній чоловік закохується в операційну систему з штучним інтелектом.",
+    rating: 8.0,
+    poster:
+      "https://i.etsystatic.com/17257718/r/il/93e590/2380694494/il_fullxfull.2380694494_yrn8.jpg",
+    genre: ["drama", "romance", "sci-fi"],
+  },
+  {
+    title: "The Social Network",
+    year: 2010,
+    description:
+      "Історія створення Facebook та конфліктів навколо його засновників.",
+    rating: 7.8,
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMjlkNTE5ZTUtNGEwNy00MGVhLThmZjMtZjU1NDE5Zjk1NDZkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    genre: ["biography", "drama"],
+  },
+  {
+    title: "Arrival",
+    year: 2016,
+    description:
+      "Лінгвістка намагається зрозуміти мову прибульців, щоб врятувати світ.",
+    rating: 7.9,
+    poster:
+      "https://i.ebayimg.com/00/s/MTYwMFgxMDM1/z/vhgAAOSwB09YK28I/$_57.JPG?set_id=8800005007",
+    genre: ["drama", "mystery", "sci-fi"],
+  },
+];
