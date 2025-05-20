8ea37950a3b037bab52ee77257c883378aa4ca71
danylyoenko <lockmaster19@gmail.com>
2025-05-20 20:27:12 +0300

    upd

--- src/js/search.js
+++ src/js/search.js
@@ -6,18 +6,3 @@ export const searchMovie = (query, movies) => {
   );
   return searchResult;
 };
-
-//   `<li class="movies-item">
-//             <article class="movie">
-//               <div class="movie-photo">
-//                 <img src="${movie.poster}" alt="Постер ${movie.title}" />
-//               </div>
-//               <div class="movie-info">
-//                 <h2 class="movie-title">${movie.title}</h2>
-//                 <p class="movie-year">${movie.year}</p>
-//                 <p class="movie-description">${movie.description}</p>
-//                 <p class="movie-ganre">${movie.genre.join(", ")}</p>
-//                 <p class="movie-rating">${movie.rating}</p>
-//               </div>
-//             </article>
-//           </li>`
